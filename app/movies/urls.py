40c97d42ec16bfeba00a0e5e39cd5ee51255d200
Riku <rikukestila@gmail.com>
2021-05-30 11:54:37 +0300

    Delete comments

--- app/movies/urls.py
+++ app/movies/urls.py
@@ -7,8 +7,3 @@ router = routers.DefaultRouter()
 router.register(r'api/movies', MovieViewSet, basename='movie')
 
 urlpatterns = router.urls
-
-"""urlpatterns = [
-    path("api/movies/", MovieList.as_view()),
-    path("api/movies/<int:pk>/", MovieDetail.as_view()),
-]"""
